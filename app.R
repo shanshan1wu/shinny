aadec0666618c16e296cffe449a886dcf27b1993
Shanshan Wu <100401155@alumnos.uc3m.es>
2019-03-06 10:09:56 +0100

    include references

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("This is a new Shiny app"),
+   titlePanel("That is a new Shiny app"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(

9610f91bee5f315c7922587308f4ea5fd5041d19
Shanshan Wu <100401155@alumnos.uc3m.es>
2019-03-06 10:37:07 +0100

    server sends list_choices

--- app.R
+++ app.R
@@ -9,43 +9,47 @@
 
 library(shiny)
 library(tidyverse)
-library(ggplot2)
 
 list_choices <-  unique(msleep$vore)[!is.na(unique(msleep$vore))]
 names(list_choices) <- paste(unique(msleep$vore)[!is.na(unique(msleep$vore))],"vore",sep="")
 
-
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("That is a new Shiny app"),
-   
-   includeMarkdown("references.md"),
-   
-   selectInput("select", label = h3("Plot by type of alimentation"), 
-               choices = list_choices,
-               selected = 1),
-   
-   plotOutput(outputId = "plot")
+ui <- fluidPage( 
+  
+  # Application title
+  titlePanel("This is a new Shiny app"),
+  
+  includeMarkdown("references.md"),
+  
+  selectInput("select", label = h3("Plot by type of alimentation"), 
+              choices = character(0),
+              selected = 1),
+  
+  plotOutput(outputId = "plot")
+  
 )
 
 col_scale <- scale_colour_discrete(limits = unique(msleep$vore))
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  # Can also set the label and select items
+  updateSelectInput(session, "select",
+                    choices = list_choices,
+                    selected = tail(list_choices, 1)
+  )
   
   output$plot <- renderPlot({
-    
-    ggplot(msleep %>% filter(vore == input$select), 
-             aes(bodywt, sleep_total, colour = vore)) +
-      scale_x_log10() +
-      col_scale+
-      geom_point()
+    # cat(file=stderr(), "input$select:", input$select == "", "\n")
+    if(input$select != ""){
+      ggplot(msleep %>% filter(vore == input$select), aes(bodywt, sleep_total, colour = vore)) +
+        scale_x_log10() +
+        col_scale +
+        geom_point()
+    }
   })
-   
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-

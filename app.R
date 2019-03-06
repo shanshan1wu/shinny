e00495de415bad7b0f9d672ce4503a90cde3d4d4
Shanshan Wu <100401155@alumnos.uc3m.es>
2019-03-06 10:18:33 +0100

    first plot

--- app.R
+++ app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -15,34 +16,22 @@ ui <- fluidPage(
    # Application title
    titlePanel("That is a new Shiny app"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+   includeMarkdown("references.md"),
+   
+   h3("Plots"),
+   
+   plotOutput(outputId = "plot")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  output$plot <- renderPlot({
+    ggplot(msleep, aes(bodywt, sleep_total, colour = vore)) +
+      scale_x_log10() +
+      geom_point() + facet_wrap(~ vore, nrow = 2)
+  })
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
 }
 
 # Run the application 
